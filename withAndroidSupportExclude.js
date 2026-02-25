cdc1a33f14ea4694574c7cf76095c5ce44eef06a
Abdulbaki Bilici <abdulbakibilici@192.168.1.45>
2026-02-26 00:02:05 +0300

    Fix EAS duplicate META-INF files error

--- withAndroidSupportExclude.js
+++ withAndroidSupportExclude.js
@@ -3,15 +3,32 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 module.exports = function withAndroidSupportExclude(config) {
     return withAppBuildGradle(config, config => {
         if (config.modResults.language === 'groovy') {
+
+            // Broaden the exclude to the whole group, removing any previous narrow excludes
+            const regex = /configurations\.all\s*\{\s*exclude group: "com\.android\.support", module: "support-compat"\s*exclude group: "com\.android\.support", module: "versionedparcelable"\s*\}/;
+            config.modResults.contents = config.modResults.contents.replace(regex, '');
+
             const snippet = `
 configurations.all {
-    exclude group: "com.android.support", module: "support-compat"
-    exclude group: "com.android.support", module: "versionedparcelable"
+    exclude group: "com.android.support"
 }
 `;
-            // Append snippet if it doesn't already exist
             if (!config.modResults.contents.includes('exclude group: "com.android.support"')) {
                 config.modResults.contents += snippet;
+            }
+
+            const packagingOptionsSnippet = `
+android {
+    packagingOptions {
+        pickFirst 'META-INF/androidx.localbroadcastmanager_localbroadcastmanager.version'
+        pickFirst 'META-INF/androidx.customview_customview.version'
+        pickFirst 'META-INF/androidx.core_core.version'
+        pickFirst 'META-INF/androidx.vectordrawable_vectordrawable.version'
+    }
+}
+`;
+            if (!config.modResults.contents.includes('pickFirst \'META-INF/androidx.localbroadcastmanager_localbroadcastmanager.version\'')) {
+                config.modResults.contents += packagingOptionsSnippet;
             }
         }
         return config;
